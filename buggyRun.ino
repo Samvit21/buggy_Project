fac130d40c38b0c73b23156481cfdc619cacfa63
Samvit21 <117395490+Samvit21@users.noreply.github.com>
2023-06-04 22:32:49 +0100

    Update buggyRun.ino

--- buggyRun.ino
+++ buggyRun.ino
@@ -1,3 +1,5 @@
+//main file that runs when program is called.
+
 #include <Servo.h>
 #include "motors.h"
 #include "servo.h"
@@ -5,7 +7,8 @@
 #include <NewPing.h>
 
 int go = 1; //bool to track status of buggy motion
-int i = 0;
+int i = 0; //bool to track number of turns made
+int j = 0; //bool that decides whether buggy should turn in response to side sensor measurements.
 int cm1 = 0; //cm1 for distance of Front sensor, cm2 for distance of Side sensor
 int cm2 = 0;
 int dist = 0; //variable to keep the previous measurement of cm2 for distance calculation
@@ -17,10 +20,6 @@ void setup(){
   pinMode(echoPinF, INPUT);
   pinMode(trigPinS, OUTPUT);
   pinMode(echoPinS, INPUT);
-//  pinMode(trigPinSF, OUTPUT);
-//  pinMode(echoPinSF, INPUT);
-//  pinMode(trigPinSB, OUTPUT);
-//  pinMode(echoPinSB, INPUT);
 
   pinMode(IN1, OUTPUT);
   pinMode(IN2, OUTPUT);
@@ -39,69 +38,49 @@ long readPing(){
 
 void loop() {
 
-//  if (state == 1){
-//    clockwise();
-//  }
-//  else if (state == 0){
-//    anticlockwise();
-//  }
-//
-//  delay(1000);
-//
-//  if (i % 2 == 0){
-//    state = 1;
-//  }
-//  else {
-//    state = 0;
-//  }
-//  i++;
+  cm1 = ultrasound(trigPinF, echoPinF);//front sensor
+  cm2 = ultrasound(trigPinS, echoPinS); //side sensor
+  
 
-  cm1 = ultrasound(trigPinF, echoPinF);
-  dist = cm2;
-  cm2 = ultrasound(trigPinS, echoPinS);
-
-// cm1 = sonar.ping_cm();
-
-  Serial.print(cm1);
-  Serial.println(" cm1");
+  Serial.print(cm2);
+  Serial.println(" cm2");
 
 
   if (cm1 >= 10){
     i = 0;
     go = 1;
     forward();
+    delay(50);
+    dist = ultrasound(trigPinS, echoPinS);
 
-    if (cm2 - dist > 10 ){
-      anticlockwise();
+    if (cm2 > 30 && j == 0){ //if there is a path to the left, and buggy is allowed to turn i.e. j == 0
+      j = 1; //This is to ensure buggy does not turn repeatedly.       
+      delay(500);//delay to ensure buggy clears the wall to its side before turning.
+      clockwise();
     }
-    else if(cm2 - dist < 10){
-      adjust_anticlock();
+    if (((cm2 - dist) > 0 || cm2 < 5 ) && j == 0){
+      adjust_anticlock(); //if buggy is moving away from wall and is allowed to turn, make a clocwise adjustment
     }
-    else if(cm2 - dist < 0);
-      adjust_clock();
-  }
+    else if ((cm2 - dist) < 0 && j == 0){
+      adjust_clock(); //if buggy is toward from wall and is allowed to turn, make a anticlocwise adjustment
+    }
+     else if (cm2 < 20){
+      j = 0;//Enables buggy to turn only if it sensed a wall previously. 
+    }
+  } 
   else if (cm1 < 10){
+    j = 0;
     go = 0;
     ++i;
-    freeze();
-    //calculate distance after turning.
+    freeze(); //Stop 
       if (i == 1){
-        clockwise();
-        delay(1000);
+        anticlockwise(); //Turn anticlockwise. 
+        delay(100);
       }
       else if (i == 2){
-        anticlockwise();
-        anticlockwise();
-        delay(1000);
-      }
-      else if (i == 3){
-        anticlockwise();
+        anticlockwise(); //Go back the way it came.
         i = 0;
-        delay(1000);
+        delay(100);
       }
   }
-
-//  if (go == 0){
-//    compare(trigPinSF, echoPinSF, trigPinSB, echoPinSB);
-//  }
 }
